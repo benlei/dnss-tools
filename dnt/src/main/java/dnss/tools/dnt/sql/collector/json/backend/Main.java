26de1fedb85d534a8cf79aa8492961b662a95519
Benjamin Lei <benlei.cse@gmail.com>
2015-05-26 01:11:02 -0700

    Changing todo message

--- dnt/src/main/java/dnss/tools/dnt/sql/collector/json/backend/Main.java
+++ dnt/src/main/java/dnss/tools/dnt/sql/collector/json/backend/Main.java
@@ -4,9 +4,9 @@ package dnss.tools.dnt.sql.collector.json.backend;
  * Created by Ben on 5/25/2015.
  */
 public class Main {
+    // TODO...
     /* Creates json files for web
      * - Levels and total SP for that level, going up to 100
-     * - skill and weapon types
      * - job table
      */
 }
